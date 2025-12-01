6fa15ed8bbb8a3252cf8b88197ea1886ffd77d5d
Abdullahi King Suleiman <abdullahiking33@gmail.com>
2025-12-01 20:16:22 +0400

    Add Capacitor iOS setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.azyah.style',
+  appName: 'Azyah Style',
+  webDir: 'dist'
+};
+
+export default config;
